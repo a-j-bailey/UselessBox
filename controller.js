bb5cfc3f8a0d8dd6a83f359b5a0475974578cdb0
Adam Bailey <33302008+adamjbailey@users.noreply.github.com>
2018-01-02 19:54:22 -0500

    Worked on JS Triggered Animations

--- controller.js
+++ controller.js
@@ -1,13 +1,22 @@
 var isOn = false;
 
-function toggle(){
-    if(isOn){
-        document.getElementById('led').classList.remove('activeLED');
-        document.getElementById('toggleSwitch').classList.remove('activeSwitch');
-        isOn = false;
-    } else {
-        document.getElementById('led').classList.add('activeLED');
-        document.getElementById('toggleSwitch').classList.add('activeSwitch');
-        isOn = true;
-    }
+function wait(ms){
+    var d = new Date();
+    var d2 = null;
+    do { d2 = new Date(); }
+    while(d2-d < ms);
+}
+
+function toggle(){  
+    //Start
+    document.getElementById('led').classList.add('activeLED');
+    document.getElementById('toggleSwitch').classList.add('activeSwitch');
+    document.getElementById('arm').classList.add('armAnimationClass');
+    
+    controller.wait(400);
+    
+    //End
+    document.getElementById('led').classList.remove('activeLED');
+    document.getElementById('toggleSwitch').classList.remove('activeSwitch');
+    document.getElementById('arm').classList.remove('armAnimationClass');
 }
