66d94d14198382bf6ec9747a754df467c2326e12
Adam Bailey <33302008+adamjbailey@users.noreply.github.com>
2018-01-03 21:09:37 -0500

    Worked on JS

--- controller.js
+++ controller.js
@@ -1,22 +1,20 @@
 var isOn = false;
 
-function wait(ms){
-    var d = new Date();
-    var d2 = null;
-    do { d2 = new Date(); }
-    while(d2-d < ms);
-}
-
-function toggle(){  
+function start(){
     //Start
     document.getElementById('led').classList.add('activeLED');
     document.getElementById('toggleSwitch').classList.add('activeSwitch');
     document.getElementById('arm').classList.add('armAnimationClass');
-    
-    controller.wait(400);
-    
+}
+
+function end(){
     //End
     document.getElementById('led').classList.remove('activeLED');
     document.getElementById('toggleSwitch').classList.remove('activeSwitch');
     document.getElementById('arm').classList.remove('armAnimationClass');
+}
+
+function toggle(){  
+    start();
+    setTimeout(end, 2000);
 }
