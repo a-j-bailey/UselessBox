9a933db6e2266c23fb7ce624f34680e9415eba1d
Adam Bailey <33302008+adamjbailey@users.noreply.github.com>
2018-01-03 21:21:05 -0500

    Timing Animations

--- controller.js
+++ controller.js
@@ -3,18 +3,19 @@ var isOn = false;
 function start(){
     //Start
     document.getElementById('led').classList.add('activeLED');
-    document.getElementById('toggleSwitch').classList.add('activeSwitch');
+    document.getElementById('toggleSwitch').classList.add('toggleAnimation1');
     document.getElementById('arm').classList.add('armAnimationClass');
 }
 
 function end(){
     //End
     document.getElementById('led').classList.remove('activeLED');
-    document.getElementById('toggleSwitch').classList.remove('activeSwitch');
+    document.getElementById('toggleSwitch').classList.add('toggleAnimation2');
     document.getElementById('arm').classList.remove('armAnimationClass');
 }
 
 function toggle(){  
     start();
     setTimeout(end, 2000);
+    //document.getElementById('toggleSwitch').classList.remove('toggleAnimation2');
 }
