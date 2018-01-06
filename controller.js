2d1ec1f0d5365b41f23e554360d1f12ab03dfc4a
Adam Bailey <33302008+adamjbailey@users.noreply.github.com>
2018-01-06 10:48:51 -0500

    JavaScript

--- controller.js
+++ controller.js
@@ -1,20 +1,20 @@
 var isOn = false;
 
-function start(){
+function start() {
     //Start
     document.getElementById('led').classList.add('activeLED');
     document.getElementById('toggleSwitch').classList.add('toggleAnimation1');
     document.getElementById('arm').classList.add('armAnimationClass');
 }
 
-function end(){
+function end() {
     //End
     document.getElementById('led').classList.remove('activeLED');
     document.getElementById('toggleSwitch').classList.add('toggleAnimation2');
     document.getElementById('arm').classList.remove('armAnimationClass');
 }
 
-function toggle(){  
+function toggle() {
     start();
     setTimeout(end, 2000);
     //document.getElementById('toggleSwitch').classList.remove('toggleAnimation2');
